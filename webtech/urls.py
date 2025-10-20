06303b6d1611a06d018f4c6b1eef1d8957c5a4f3
violetta <kvitta55555@gmail.com>
2025-10-20 20:25:30 +0600

    лаб4

--- webtech/urls.py
+++ webtech/urls.py
@@ -20,8 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('guestbook/', include('guestbook.urls'))
-    path('accounts/', include('django.contrib.auth.urls')),  # login, logout, password_change, etc.
+    path('accounts/logout', include('django.contrib.auth.urls')),  # login, logout, password_change, etc.
     path('', include('guestbook.urls')),  
 ]
 
